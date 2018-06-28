3a6842de78ea9a90f62ef5f642be97f0acb7636f
MojitoBar <wnehdtjr5@naver.com>
2018-06-28 22:54:16 +0900

    Building the Weather View

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import Weather from "./weather";
 
 export default class App extends Component {
   state = {
-    isLoaded: false
+    isLoaded: true
   }
   render() {
     const { isLoaded } = this.state;
     return (
       <View style={styles.container}>
-        {isLoaded ? null : (
+        {isLoaded ? (
+          <Weather/>
+        ) : (
           <View style = {styles.loading}>
-            <Text style = {styles.loadingText}>Getting the fucking weather</Text>
+            <Text style = {styles.loadingText}>
+              Getting the fucking weather
+            </Text>
           </View>
         )}
       </View>
