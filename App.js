9b74c41036db5e19793ab11b042c408b420574d5
MojitoBar <wnehdtjr5@naver.com>
2018-06-28 22:26:48 +0900

    Building the Loading View

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  state = {
+    isLoaded: false
+  }
   render() {
+    const { isLoaded } = this.state;
     return (
       <View style={styles.container}>
-      <ActivityIndicator/>
-        <Text>Ok now i'm native developer and i loooooooooooove REACT!!!</Text>
+        {isLoaded ? null : (
+          <View style = {styles.loading}>
+            <Text style = {styles.loadingText}>Getting the fucking weather</Text>
+          </View>
+        )}
       </View>
     );
   }
@@ -15,8 +22,16 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
+  loading: {
+    flex: 1,
+    backgroundColor: '#FDF6AA',
+    justifyContent: "flex-end",
+    paddingLeft: 25
+  },
+  loadingText:{
+    fontSize: 38,
+    marginBottom: 100
+  }
 });
